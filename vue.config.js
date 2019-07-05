365873ed4219b715b6aeb89fcdc6fb7a5b471526
xaq <3895965156@qq.com>
2019-07-05 16:49:57 +0800

    【仓储】- 需求修改

--- vue.config.js
+++ vue.config.js
@@ -37,11 +37,12 @@ module.exports = {
   // 设为false打包时不生成.map文件
   productionSourceMap: false,
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
+  //target: "http://10.0.3.231:18761",
   devServer:{
     port:8080,
     proxy:{
       '/api' : {
-        target: "http://172.16.10.23:8089",
+        target: "http://192.168.0.8:18761",
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/api'
